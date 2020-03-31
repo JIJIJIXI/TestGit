4d23b83ec6da2b53194001c9fa4ca2e5512ea2cd
jijixi <924974944@qq.com>
2020-03-31 16:41:17 +0800

    test conflict again

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -1,8 +1,11 @@
 package com.atguigu.git;
 
+//TestGit--down
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("down....");
 		System.out.println("up....");
+		
+		System.out.println("push after TestGit");
 	}
 }
