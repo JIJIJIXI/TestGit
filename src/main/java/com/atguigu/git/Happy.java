d51d888ac0ad63f66ae7aa7afc1f613b860401ad
jijixi <924974944@qq.com>
2020-03-31 16:39:57 +0800

    test conflict again

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,7 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("down....");
 		System.out.println("up....");
+		
+		System.out.println("test conflict again");
 	}
 }
